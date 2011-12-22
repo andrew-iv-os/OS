45972b840dfb00e8f8afd068912d36786f8d890d
Andrey <andrew-iv@hackerdom.ru>
2011-12-23 05:14:01 +0600

    TASK 6

---
+++ Task6/task6.c
@@ -0,0 +1,16 @@
+void print(){
+	*(short*)0xB8000 = 0x0700 + 'M';
+	//return;
+
+short *mem;
+mem = 0xB8000;
+int i;
+for( i =0 ;i <0x400 ; i++)
+	{
+		*mem = 0x0733;
+		mem++;		
+	};
+	
+while(1);
+
+}
